492a3d4132b44a844dfa2d4dc9477f446eb2b543
Noam Mendoza <noamalfredo@gmail.com>
2025-04-05 17:36:09 -0600

    Agregado el microservicio de estrenos

---
+++ estrenos/src/main/java/com/estrenos/estrenos/EstrenosApplication.java
@@ -0,0 +1,13 @@
+package com.estrenos.estrenos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstrenosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstrenosApplication.class, args);
+	}
+
+}
---
+++ estrenos/src/test/java/com/estrenos/estrenos/EstrenosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estrenos.estrenos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstrenosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
